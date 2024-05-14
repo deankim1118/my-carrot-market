55566f1e311788330fb5b8c9dafdc301e8fede00
Dean Kim <deankim1118@gmail.com>
2024-05-13 22:34:34 -0400

    1. Implement Authentication UI

--- my-carrot-market/app/page.tsx
+++ my-carrot-market/app/page.tsx
@@ -1,5 +1,25 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className='flex flex-col items-center justify-between min-h-screen p-6'>
+      <div className='my-auto flex flex-col items-center gap-2 *:font-medium'>
+        <span className='text-9xl'>🥕</span>
+        <h1 className='text-4xl'>Carrot</h1>
+        <h2 className='text-2xl'>Welcome Carrot Market</h2>
+      </div>
+      <div className='flex flex-col items-center gap-3 w-full'>
+        <Link href='/create-account' className='primary-btn py-2.5 text-lg'>
+          Start
+        </Link>
+        <div className='flex gap-2 '>
+          <span>Already have an account?</span>
+          <Link href='/login' className=' hover:underline underline-offset-4'>
+            Login
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
