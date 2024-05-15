0990cdf0ceaf206e72146b9ddac029b15aebe27d
Dean Kim <deankim1118@gmail.com>
2024-05-15 16:38:25 -0400

    2-3. Custom & Refine Validation

--- my-carrot-market/app/create-account/actions.ts
+++ my-carrot-market/app/create-account/actions.ts
@@ -1,12 +1,25 @@
 'use server';
 import { z } from 'zod';
 
-const formSchema = z.object({
-  username: z.string().min(3),
-  email: z.string().email(),
-  password: z.string().min(6),
-  confirmPassword: z.string().min(6),
-});
+const formSchema = z
+  .object({
+    username: z
+      .string({ invalid_type_error: 'Username should contain the text!' })
+      .min(3, 'Username must contain at least 3 characters')
+      .refine(
+        (username) => !username.includes('fuck'),
+        'You can not use F word'
+      ),
+    email: z.string().email(),
+    password: z.string().min(6, 'Password must contain at least 6 characters'),
+    confirmPassword: z
+      .string()
+      .min(6, 'Password must contain at least 6 characters'),
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: 'Both password must be same',
+    path: ['confirmPassword'],
+  });
 
 export const createAccount = async (prevState: any, formData: FormData) => {
   const data = {
@@ -17,6 +30,7 @@ export const createAccount = async (prevState: any, formData: FormData) => {
   };
   const result = formSchema.safeParse(data);
   if (!result.success) {
+    console.log(result.error.flatten());
     return result.error.flatten();
   }
 };
