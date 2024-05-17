81600812717de84b3c6a6cb74664baecb205f63d
Dean Kim <deankim1118@gmail.com>
2024-05-17 15:00:35 -0400

    3-4. Auth - Email Login

--- my-carrot-market/app/login/actions.ts
+++ my-carrot-market/app/login/actions.ts
@@ -5,14 +5,36 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from '@/lib/constants';
+import db from '@/lib/db';
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import getSession from '@/lib/session';
+import { redirect } from 'next/navigation';
+
+const checkEmailExist = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return Boolean(user);
+};
 
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
-  password: z
+  email: z
     .string()
-    .min(PASSWORD_MIN_LENTH)
-    .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+    .email()
+    .toLowerCase()
+    .refine(
+      checkEmailExist,
+      'We cannot find a email you entered plaese use create account'
+    ),
+  password: z.string().min(PASSWORD_MIN_LENTH),
+  //.regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 });
 
 export const login = async (prevState: any, formData: FormData) => {
@@ -20,11 +42,40 @@ export const login = async (prevState: any, formData: FormData) => {
     email: formData.get('email'),
     password: formData.get('password'),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
 
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    // 1. find a user with the email
+    /// You can do this in validation of zod.refine(boolean, errorMessage) ///
+    // 2. if the user is found, check password hashed
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+    const ok = await bcrypt.compare(result.data.password, user!.password ?? '');
+    if (ok) {
+      // 3. log the uesr in
+      const session = await getSession();
+      session.id = user!.id;
+      await session.save();
+      redirect('/profile');
+    } else {
+      return {
+        //  Pretend to be Zod & Make same shape of zod.error.flatten() to send the error message
+        fieldErrors: {
+          password: ['Wrong password'],
+          email: [],
+        },
+      };
+    }
+
+    // 4. redirect '/profile'
   }
 };
