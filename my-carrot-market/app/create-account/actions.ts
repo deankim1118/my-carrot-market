eabbd3971e1abee234f52a1c49697a5b873ebcc1
Dean Kim <deankim1118@gmail.com>
2024-05-17 12:10:39 -0400

    3-1. Validate User Model Database

--- my-carrot-market/app/create-account/actions.ts
+++ my-carrot-market/app/create-account/actions.ts
@@ -5,6 +5,7 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from '@/lib/constants';
+import db from '@/lib/db';
 import { z } from 'zod';
 
 const checkPassword = ({
@@ -17,17 +18,48 @@ const checkPassword = ({
   return password === confirmPassword;
 };
 
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  // if (user) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
     username: z
       .string({ invalid_type_error: 'Username should contain the text!' })
       .toLowerCase()
       .trim()
-      .refine(
-        (username) => !username.includes('fuck'),
-        'You can not use F word'
-      ),
-    email: z.string().email().toLowerCase(),
+      .refine(checkUniqueUsername, 'This Username is already taken.'),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .refine(checkUniqueEmail, 'This Email is already taken.'),
     password: z
       .string()
       .min(PASSWORD_MIN_LENTH)
@@ -46,11 +78,20 @@ export const createAccount = async (prevState: any, formData: FormData) => {
     password: formData.get('password'),
     confirmPassword: formData.get('confirmPassword'),
   };
-  const result = formSchema.safeParse(data);
+
+  const result = await formSchema.safeParseAsync(data);
+
   if (!result.success) {
     console.log(result.error.flatten());
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    // 1. check if username is taken & show an error message
+    /// You can do this in validation of zod.refine(boolean, errorMessage) ///
+    // 2. ckeck if email is already used
+    /// You can do this in validation of zod.refine(boolean, errorMessage) ///
+    // 3. hash password
+    // 4. save the user to db
+    // 5. log the user in
+    // 6. redirect to '/home'
   }
 };
