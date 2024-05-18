dc57365fcab7b6ee2fd55d7dbe34d7dd37a72554
Dean Kim <deankim1118@gmail.com>
2024-05-18 14:21:22 -0400

    3-6. Auth - Middleware to redirect

---
+++ my-carrot-market/middleware.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server';
+import getSession from './lib/session';
+
+interface IRoutes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: IRoutes = {
+  '/': true,
+  '/login': true,
+  '/sms': true,
+  '/create-account': true,
+};
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      return Response.redirect(new URL('/login', request.url));
+    }
+  } else {
+    if (exists) {
+      return Response.redirect(new URL('/profile', request.url));
+    }
+  }
+}
+export const config = {
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   */
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
