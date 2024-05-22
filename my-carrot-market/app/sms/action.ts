2f4a63ed63263e06b8823c7560fcecb39df8aed5
Dean Kim <deankim1118@gmail.com>
2024-05-22 12:54:08 -0400

    5-1. SMS Auth - Create Token

--- my-carrot-market/app/sms/action.ts
+++ my-carrot-market/app/sms/action.ts
@@ -1,8 +1,10 @@
 'use server';
 
+import crypto from 'crypto';
 import { z } from 'zod';
 import validator from 'validator';
 import { redirect } from 'next/navigation';
+import db from '@/lib/db';
 
 const phoneSchema = z
   .string()
@@ -19,6 +21,21 @@ interface ActionState {
   token: boolean;
 }
 
+async function getToken() {
+  const token = crypto.randomInt(100000, 999999).toString();
+  const exists = await db.sMSToken.findUnique({
+    where: { token },
+    select: {
+      id: true,
+    },
+  });
+  if (exists) {
+    return getToken();
+  } else {
+    return token;
+  }
+}
+
 export async function smsLogin(prevState: ActionState, formData: FormData) {
   const phone = formData.get('phone');
   const token = formData.get('token');
@@ -27,6 +44,29 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
     if (!result.success) {
       return { token: false, error: result.error.flatten() };
     } else {
+      // 1. delete previous token
+      await db.sMSToken.deleteMany({
+        where: { user: { phone: result.data } },
+      });
+      // 2. create new token
+      const token = await getToken();
+      await db.sMSToken.create({
+        data: {
+          token,
+          user: {
+            connectOrCreate: {
+              where: {
+                phone: result.data,
+              },
+              create: {
+                username: crypto.randomBytes(10).toString('hex'),
+                phone: result.data,
+              },
+            },
+          },
+        },
+      });
+      // 3. send the token using twilio
       return { token: true };
     }
   } else {
